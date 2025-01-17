7ab70213d9c6efc0b7d4d2c68fe6d58c7d981130
mbullr <sd@nextjobsolutions.com>
2025-01-17 16:04:10 -0600

    update install script to use whatever python installed
    update install script to use whatever python installed
    added command WHO.AM.I

--- deletesd.sh
+++ deletesd.sh
@@ -5,6 +5,7 @@
 #	This software is released under the Blue Oak Model License
 #	a copy can be found on the web here: https://blueoakcouncil.org/license/1.0.0
 #
+#   rev 0.9.0 Jan 25 mab must remove group sdsys (no longer sdsys's pri group)
 #
     if [[ $EUID -eq 0 ]]; then
        echo "This script must NOT be run as root" 1>&2
@@ -82,6 +83,8 @@ echo "Removed systemd service files."
 # remove sdsys user and sdusers group
 sudo userdel sdsys
 sudo groupdel sdusers
+sudo groupdel sdsys
+
 echo "Removed sdsys user and sdusers group."
 
 echo
